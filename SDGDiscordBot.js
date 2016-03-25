6de6f54b216d8e04040aa9f59ba88c90f65dd7ec
Quinten S <cqsimmons87@gmail.com>
2016-03-25 01:07:36 -0400

    Added broken table functionality
    When table catcher reaches his maximum state the table breaks and there
    is a 5 minute cooldown timer before that channel will be able to catch
    tables again.

--- SDGDiscordBot.js
+++ SDGDiscordBot.js
@@ -21,6 +21,7 @@ function TableCatcher(channel){
 	this.emotionalState = ["┬─┬ノ( ゜-゜ノ)", "┬─┬ノ(ಠ益ಠノ)", "┬─┬ノ(ಥ益ಥノ)", "(/ .□.)\ ︵╰(゜Д゜)╯︵ /(.□. \)"]
 	this.lastFlipTimestamp = new Date();
 	this.channel = channel; 
+	this.tableBroken = false;
 }
 var tableCatcherArray = new Array(); //Keeps all TableCatcher objects
 
@@ -46,46 +47,66 @@ bot.on("message", function(message)
 	}
 	
 	if(message.content.includes("(╯°□°）╯︵ ┻━┻")){
+		var channelHasCatcher = false;
 		if (tableCatcherArray.length > 0){ //Make sure there's at least 1 object in the array
-			var channelHasCatcher = false;
 			for (var i = 0; i < tableCatcherArray.length; i++){ //loop through array to see if object already exists for channel
-				var currentTableCatcher = tableCatcherArray[i];
-				if (currentTableCatcher.channel.equals(message.channel)){
-					channelHasCatcher = true;
-					if (Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) <= 30000){
+				var currentTableCatcher = tableCatcherArray[i]; //set new var to current array object
+				if (currentTableCatcher.channel.equals(message.channel)){//if the we find an object already exists for the channel
+					channelHasCatcher = true;//set flag to true that channel already has object
+					if (currentTableCatcher.tableBroken == true && Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) < 300000){//if table is broken and it's been less than 5 minutes
+						bot.reply(message, "*TABLE SHATTERS*: Shamebot's sick of your shit. He'll be back to save the tables in a few minutes.")
+						console.log("10");
+					}else if (currentTableCatcher.tableBroken == true && Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) >= 300000){//if table is broken but it's been 5 minutes or more
+						currentTableCatcher.tableBroken = false;
 						bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+						console.log("9");
 						currentTableCatcher.lastFlipTimestamp = new Date();
-						if (currentTableCatcher.currentEmotionalState <= tableCatcherArray.length - 2){
-							currentTableCatcher.currentEmotionalState++;
-						} else {
-							currentTableCatcher.currentEmotionalState = 0;
-						}
-					}else if (Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) > 30000){
-						var timePast = Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp);
-						var numberOfIncrementsPast = (timePast / 30000) - 1; //30 seconds is one increment
-						
-						if (numberOfIncrementsPast < 1){
-							//return previous table catch emotion
-							currentTableCatcher.currentEmotionalState--;
+						currentTableCatcher.currentEmotionalState++;
+					}else{//else -> table is not broken...
+						if (Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) <= 30000){
 							bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+							console.log("8");
 							currentTableCatcher.lastFlipTimestamp = new Date();
-						}else {
-							//decrease emotional states equal to the number of increments to a minimum of 0
-							numberOfIncrementsPast = Math.floor(numberOfIncrementsPast);
-							if (currentTableCatcher.currentEmotionalState - numberOfIncrementsPast <= 0){
-								currentTableCatcher.currentEmotionalState = 0;
-								bot.reply(message, currentTableCatcher.emotionalState[0]);
+							if (currentTableCatcher.currentEmotionalState <= currentTableCatcher.emotionalState.length - 2){
+								currentTableCatcher.currentEmotionalState++;
 							}else {
-								currentTableCatcher.currentEmotionalState -=  numberOfIncrementsPast;
-								bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+								//bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+								console.log("7");
+								currentTableCatcher.currentEmotionalState = 0;
+								currentTableCatcher.tableBroken = true;
 							}
-						}
-						
-						if (currentTableCatcher.currentEmotionalState <= tableCatcherArray.length - 2){
-							currentTableCatcher.currentEmotionalState++;
-						} else {
-							currentTableCatcher.currentEmotionalState = 0;
-							bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+						}else if (Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp) > 30000){
+							var timePast = Math.abs(new Date() - currentTableCatcher.lastFlipTimestamp);
+							var numberOfIncrementsPast = (timePast / 30000) - 1; //30 seconds is one increment
+							
+							if (numberOfIncrementsPast < 1){
+								//return previous table catch emotion
+								currentTableCatcher.currentEmotionalState--;
+								bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+								console.log("6");
+								currentTableCatcher.lastFlipTimestamp = new Date();
+							}else {
+								//decrease emotional states equal to the number of increments to a minimum of 0
+								numberOfIncrementsPast = Math.floor(numberOfIncrementsPast);
+								if (currentTableCatcher.currentEmotionalState - numberOfIncrementsPast <= 0){
+									currentTableCatcher.currentEmotionalState = 0;
+									bot.reply(message, currentTableCatcher.emotionalState[0]);
+									console.log("5");
+								}else {
+									currentTableCatcher.currentEmotionalState -=  numberOfIncrementsPast;
+									bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+									console.log("4");
+								}
+							}
+							
+							if (currentTableCatcher.currentEmotionalState <= currentTableCatcher.emotionalState.length - 2){
+								currentTableCatcher.currentEmotionalState++;
+							}else {
+								bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+								console.log("3");
+								currentTableCatcher.currentEmotionalState = 0;
+								currentTableCatcher.tableBroken = true;
+							}
 						}
 					}
 				}
@@ -94,6 +115,7 @@ bot.on("message", function(message)
 				tableCatcherArray.push(new TableCatcher(message.channel));
 				var currentTableCatcher = tableCatcherArray[tableCatcherArray.length - 1];
 				bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+				console.log("2");
 				currentTableCatcher.lastFlipTimestamp = new Date();
 				currentTableCatcher.currentEmotionalState++;
 			}
@@ -101,6 +123,7 @@ bot.on("message", function(message)
 			tableCatcherArray[0] = new TableCatcher(message.channel);
 			var currentTableCatcher = tableCatcherArray[0];
 			bot.reply(message, currentTableCatcher.emotionalState[currentTableCatcher.currentEmotionalState]);
+			console.log("1");
 			currentTableCatcher.lastFlipTimestamp = new Date();
 			currentTableCatcher.currentEmotionalState++;
 		}
