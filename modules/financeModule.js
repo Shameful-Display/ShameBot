bf28c8bdf0604f4c07674f225096e730a64d1bba
David J King <davidjking@me.com>
2020-05-17 17:09:17 -0400

    Embed message support

--- modules/financeModule.js
+++ modules/financeModule.js
@@ -2,19 +2,19 @@ var fs = require( "fs" );
 var path = require( "path" );
 var process = require( "process" );
 var request = require("request");
-var querystring = require('querystring')
+var querystring = require('querystring');
+var winston = require('winston');
 var AuthDetails = require("../auth.json");
 const { Client, MessageEmbed } = require('discord.js');
+
+const greenColor = "0x33FF00"
+const redColor = "0xCC0000"
+const neutralColor = "0x99FFFF"
 
 const rapidAPIURL = "https://alpha-vantage.p.rapidapi.com/query";
 const rapidAPIHost = "alpha-vantage.p.rapidapi.com";
 
-var financeEmbedBuilder = function (symbol, message) {
-  var greenColor = "0x33FF00"
-  var redColor = "0xCC0000"
-  var neutralColor = "0x99FFFF"
-  var color = neutralColor;
-
+var financeEmbedBuilder = function (symbol, message, color) {
   const embed = new MessageEmbed()
     .setTitle(symbol)
     .setColor(color)
@@ -40,8 +40,18 @@ var FinanceManager = function (bot) {
 
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        message.reply(financeEmbedBuilder(symbol, body));
-      });
+        var infoList = JSON.parse(body);
+          var symbol = infoList["Global Quote"]["01. symbol"];
+          var price = infoList["Global Quote"]["05. price"];
+          var change = parseFloat(infoList["Global Quote"]["10. change percent"]);
+          var stockColor = neutralColor;
+          if (change > 0){
+            stockColor = greenColor;
+          } else {
+            stockColor = redColor;
+          }
+          message.reply(financeEmbedBuilder(symbol + " Stock Info", "**" + symbol + "** \n" + "$" + price + "\n" + change.toString() + "%" + "\n", stockColor));
+      }).catch((err) => winston.error("couldn't find stock", err));
   }
 }
 
