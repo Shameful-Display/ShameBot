b5572626bae2c1f7b6fcf1cef08cbaee8ef7a45a
David J King <davidjking@me.com>
2020-05-17 17:43:23 -0400

    Upgrading from Request to Axios
    Request is deprecated. Much easier object management with axios anyway

--- modules/financeModule.js
+++ modules/financeModule.js
@@ -1,11 +1,11 @@
 var fs = require( "fs" );
 var path = require( "path" );
 var process = require( "process" );
-var request = require("request");
 var querystring = require('querystring');
 var winston = require('winston');
 var AuthDetails = require("../auth.json");
 const { Client, MessageEmbed } = require('discord.js');
+const axios = require("axios");
 
 const greenColor = "0x33FF00"
 const redColor = "0xCC0000"
@@ -25,33 +25,37 @@ var financeEmbedBuilder = function (symbol, message, color) {
 var FinanceManager = function (bot) {
   this.stockInfo = function (message) {
     bot.user.setPresence({ activity: { name: "the stock market" , type: 0} });
-    var symbol = querystring.escape(message.cleanContent.replace('!stock','')).toString().substr(3);
+    var company = querystring.escape(message.cleanContent.replace('!stock','')).toString().substr(3);
 
-    var options = {
-      method: 'GET',
-      url: rapidAPIURL,
-      qs: {symbol: symbol, function: 'GLOBAL_QUOTE'},
-      headers: {
-        'x-rapidapi-host': rapidAPIHost,
-        'x-rapidapi-key': AuthDetails.financeKey,
-        useQueryString: true
-      }
-    };
-
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        var infoList = JSON.parse(body);
-          var symbol = infoList["Global Quote"]["01. symbol"];
-          var price = infoList["Global Quote"]["05. price"];
-          var change = parseFloat(infoList["Global Quote"]["10. change percent"]);
-          var stockColor = neutralColor;
-          if (change > 0){
-            stockColor = greenColor;
-          } else {
-            stockColor = redColor;
-          }
-          message.reply(financeEmbedBuilder(symbol + " Stock Info", "**" + symbol + "** \n" + "$" + price + "\n" + change.toString() + "%" + "\n", stockColor));
-      }).catch((err) => winston.error("couldn't find stock", err));
+    axios({
+        "method": "GET",
+        "url": rapidAPIURL,
+        "headers":{
+            "content-type":"application/octet-stream",
+            "x-rapidapi-host":rapidAPIHost,
+            "x-rapidapi-key":AuthDetails.financeKey,
+            "useQueryString":true},
+        "params":{
+            "symbol":company,
+            "function":"GLOBAL_QUOTE"}
+        })
+        .then((response)=>{
+            var infoList = response.data;
+            var symbol = infoList["Global Quote"]["01. symbol"];
+            var price = infoList["Global Quote"]["05. price"];
+            var change = parseFloat(infoList["Global Quote"]["10. change percent"]);
+            var stockColor = neutralColor;
+            if (change > 0){
+              stockColor = greenColor;
+            } else {
+              stockColor = redColor;
+            }
+            message.reply(financeEmbedBuilder(symbol + " Stock Info", "**" + symbol + "** \n" + "$" + price + "\n" + change.toString() + "%" + "\n", stockColor));
+        })
+        .catch((error)=>{
+          winston.error("Finance Error: ", error);
+          message.reply("Couldn't find '" + company + "'...maybe you shouldn't be trading if you don't know the symbol.");
+        })
   }
 }
 
